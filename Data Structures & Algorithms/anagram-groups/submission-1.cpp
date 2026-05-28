5e6d0c5ce50397eec47b7f5e625a3afdcd62b0fa
Rohit Saini <rohitxsaini6@gmail.com>
2026-05-28 13:12:20 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> hash;
+
+        for (string str: strs) {
+            string temp = str;
+            sort(temp.begin(), temp.end());
+            hash[temp].push_back(str);
+        }
+
+        vector<vector<string>> vec;
+        for (auto it : hash) {
+            vec.push_back(it.second);
+        }
+
+        return vec;
+    }
+};
