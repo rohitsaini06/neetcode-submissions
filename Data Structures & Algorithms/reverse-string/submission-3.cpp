f3b5043fe3ac85c439dcbd2ac25666afa9fe9ba0
Rohit Saini <rohitxsaini6@gmail.com>
2026-06-07 15:53:27 +0530

    Add: reverse-string - submission-3

---
+++ Data Structures & Algorithms/reverse-string/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        // int i = 0;
+        // int j = s.size()-1;
+        // char temp;
+        // while (i <= j) {
+        //     temp = s[i];
+        //     s[i] = s[j];
+        //     s[j] = temp;
+        //     i++;
+        //     j--;
+        // }
+
+        reverse(s.begin(), s.end());
+    }
+};
